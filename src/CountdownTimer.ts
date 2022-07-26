3eda7b17046f4f81afb7ef9373eda810c72fdb59
cbserra <789723+cbserra@users.noreply.github.com>
2022-07-26 07:13:17 -0700

    feat: wip card flip clock

---
+++ src/CountdownTimer.ts
@@ -0,0 +1,164 @@
+import {
+  CountdownUnitElements,
+  CountdownUnitValues,
+  toDaysFormatted,
+  toHoursFormatted,
+  toMinutesFormatted,
+  toSecondsFormatted,
+  DeltaValues,
+  setAttribute,
+  getUpdatedDistance,
+} from "./util";
+
+const DATA_VAL_ATTR = "data-value";
+
+const TOGGLE_ANIMATION_CLASS = "flip";
+
+export class CountdownTimer {
+  private readonly targetDate: number;
+  private currentDate: number;
+  //   private distance: number;
+  private unitElements: CountdownUnitElements;
+  private unitValues: CountdownUnitValues;
+
+  constructor(
+    targetDate: number,
+    currentDate: number,
+    unitElements: CountdownUnitElements,
+    unitValues: CountdownUnitValues
+  ) {
+    this.targetDate = targetDate;
+    this.currentDate = currentDate || Date.now();
+    // this.distance = getUpdatedDistance(this.targetDate);
+    this.unitElements = unitElements;
+    this.unitValues = unitValues;
+
+    this._init();
+  }
+
+  /**
+   * Getter $targetDate
+   * @return {number}
+   */
+  public get $targetDate(): number {
+    return this.targetDate;
+  }
+
+  /**
+   * Getter $currentDate
+   * @return {number}
+   */
+  public get $currentDate(): number {
+    return this.currentDate;
+  }
+
+  /**
+   * Setter $currentDate
+   * @param {number} value
+   */
+  public set $currentDate(value: number) {
+    this.currentDate = value;
+  }
+
+  //   /**
+  //    * Setter $distance
+  //    * @param {number} value
+  //    */
+  //   public set $distance(value: number) {
+  //     this.distance = value;
+  //   }
+
+  //   /**
+  //    * Getter $distance
+  //    * @return {number}
+  //    */
+  //   public get $distance(): number {
+  //     return this.distance;
+  //   }
+
+  //   public update(distance?: number): void {
+  public update(): void {
+    console.count("this.update");
+    const timeInterval = requestAnimationFrame(() => this.update());
+
+    // this.distance = distance ?? getUpdatedDistance(this.targetDate);
+    const distance = getUpdatedDistance(this.targetDate);
+
+    if (distance <= 0) {
+      cancelAnimationFrame(timeInterval);
+      return;
+    }
+
+    this._updateTimeUnits(distance);
+
+    this._updateCountdownUi();
+  }
+
+  private _init(): void {
+    // this.update();
+  }
+
+  private _updateTimeUnits(distance: number) {
+    // this.distance = distance ?? getUpdatedDistance(this.targetDate);
+    this.unitValues.days = this._getUpdatedCountdownValues(
+      this.unitValues.days,
+      toDaysFormatted(distance)
+    );
+    this.unitValues.hours = this._getUpdatedCountdownValues(
+      this.unitValues.hours,
+      toHoursFormatted(distance)
+    );
+    this.unitValues.minutes = this._getUpdatedCountdownValues(
+      this.unitValues.minutes,
+      toMinutesFormatted(distance)
+    );
+    this.unitValues.seconds = this._getUpdatedCountdownValues(
+      this.unitValues.seconds,
+      toSecondsFormatted(distance)
+    );
+  }
+
+  private _updateCountdownUi(): void {
+    Object.keys(this.unitElements).forEach((unit) => {
+      const elements = this.unitElements[unit as keyof CountdownUnitElements];
+      const values = this.unitValues[unit as keyof CountdownUnitValues];
+      if (elements.topElement && values.currentValue !== values.previousValue) {
+        elements.topElement.innerText = values.previousValue;
+        setAttribute(
+          elements.bottomElement,
+          DATA_VAL_ATTR,
+          values.currentValue
+        );
+        setAttribute(elements.backElement, DATA_VAL_ATTR, values.currentValue);
+        setAttribute(
+          elements.backBottomElement,
+          DATA_VAL_ATTR,
+          values.previousValue
+        );
+
+        this._toggleAnimationClass(elements.outerContainer as HTMLElement);
+
+        // setTimeout(this.update, 1_000);
+        // setTimeout(() => this.update(), 500);
+        // prevDays = currentDays;
+      }
+    });
+  }
+  private _toggleAnimationClass(outerContainer: HTMLElement): void {
+    outerContainer.classList.remove(TOGGLE_ANIMATION_CLASS);
+    // Updates 'offsetWidth' to force a reflow
+    void outerContainer.offsetWidth;
+    outerContainer.classList.add(TOGGLE_ANIMATION_CLASS);
+  }
+
+  private _getUpdatedCountdownValues(
+    deltaValues: DeltaValues,
+    formattedValue: string
+  ): DeltaValues {
+    const updatedDeltas = {
+      previousValue: deltaValues.currentValue,
+      currentValue: formattedValue,
+    };
+    return updatedDeltas;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,150 +1,58 @@
-// import "./style.css";
+import { CountdownTimer } from "./CountdownTimer";
+import {
+  TimeUnitKey,
+  getUpdatedDistance,
+  CountdownUnitElements,
+  CountdownUnitValues,
+  INIT_DELTA_VALUES,
+  getCountdownElements,
+  TWO_WEEKS_AHEAD,
+} from "./util";
 
-const COUNT_DOWN_INTERVAL = 1000;
-const HOURS_IN_DAY = 24;
-const MINUTES_IN_HOUR = 60;
-const SECONDS_IN_MINUTE = 60;
-const MILLIS_IN_SECOND = 1000;
-const MILLIS_IN_DAY =
-  MILLIS_IN_SECOND * SECONDS_IN_MINUTE * MINUTES_IN_HOUR * HOURS_IN_DAY;
-const MILLIS_IN_HOUR = MILLIS_IN_SECOND * SECONDS_IN_MINUTE * MINUTES_IN_HOUR;
-const MILLIS_IN_MINUTE = MILLIS_IN_SECOND * SECONDS_IN_MINUTE;
-
-const daysEl = document.getElementById("days");
-console.log(`🚀 ~ file: index.ts ~ line 15 ~ daysEl`, daysEl);
-const daysBottomEl = daysEl?.parentElement?.querySelector(".time-unit__bottom");
-console.log(`🚀 ~ file: index.ts ~ line 17 ~ daysBottomEl`, daysBottomEl);
-const daysBackEl = daysEl?.parentElement?.querySelector(".time-unit__back");
-console.log(`🚀 ~ file: index.ts ~ line 19 ~ daysBackEl`, daysBackEl);
-const daysBackBottomEl = daysBackEl?.querySelector(".time-unit__bottom");
-console.log(
-  `🚀 ~ file: index.ts ~ line 21 ~ daysBackBottomEl`,
-  daysBackBottomEl
-);
-
-const hoursEl = document.getElementById("hours");
-console.log(`🚀 ~ file: index.ts ~ line 55 ~ hoursEl`, hoursEl);
-const hoursBottomEl =
-  hoursEl?.parentElement?.querySelector(".time-unit__bottom");
-console.log(`🚀 ~ file: index.ts ~ line 55 ~ hoursBottomEl`, hoursBottomEl);
-const hoursBackEl = hoursEl?.parentElement?.querySelector(".time-unit__back");
-console.log(`🚀 ~ file: index.ts ~ line 55 ~ hoursBackEl`, hoursBackEl);
-const hoursBackBottomEl = hoursBackEl?.querySelector(".time-unit__bottom");
-console.log(
-  `🚀 ~ file: index.ts ~ line 55 ~ hoursBackBottomEl`,
-  hoursBackBottomEl
-);
-
-const minutesEl = document.getElementById("minutes");
-console.log(`🚀 ~ file: index.ts ~ line 55 ~ minutesEl`, minutesEl);
-const minutesBottomEl =
-  minutesEl?.parentElement?.querySelector(".time-unit__bottom");
-console.log(`🚀 ~ file: index.ts ~ line 55 ~ minutesBottomEl`, minutesBottomEl);
-const minutesBackEl =
-  minutesEl?.parentElement?.querySelector(".time-unit__back");
-console.log(`🚀 ~ file: index.ts ~ line 55 ~ minutesBackEl`, minutesBackEl);
-const minutesBackBottomEl = minutesBackEl?.querySelector(".time-unit__bottom");
-console.log(
-  `🚀 ~ file: index.ts ~ line 55 ~ minutesBackBottomEl`,
-  minutesBackBottomEl
-);
-
-const secondsEl = document.getElementById("seconds");
-console.log(`🚀 ~ file: index.ts ~ line 55 ~ secondsEl`, secondsEl);
-const secondsBottomEl =
-  secondsEl?.parentElement?.querySelector(".time-unit__bottom");
-console.log(`🚀 ~ file: index.ts ~ line 55 ~ secondsBottomEl`, secondsBottomEl);
-const secondsBackEl =
-  secondsEl?.parentElement?.querySelector(".time-unit__back");
-console.log(`🚀 ~ file: index.ts ~ line 55 ~ secondsBackEl`, secondsBackEl);
-const secondsBackBottomEl = secondsBackEl?.querySelector(".time-unit__bottom");
-console.log(
-  `🚀 ~ file: index.ts ~ line 55 ~ secondsBackBottomEl`,
-  secondsBackBottomEl
-);
-
-const addDays = (date: Date, days: number) => {
-  const newDate = new Date(date.valueOf());
-  newDate.setDate(newDate.getDate() + days);
-  return newDate;
+const INIT_UNIT_DELTA_VALUES: CountdownUnitValues = {
+  [TimeUnitKey.days]: INIT_DELTA_VALUES,
+  [TimeUnitKey.hours]: INIT_DELTA_VALUES,
+  [TimeUnitKey.minutes]: INIT_DELTA_VALUES,
+  [TimeUnitKey.seconds]: INIT_DELTA_VALUES,
 };
 
-const runCountdownTimer = () => {
-  let prevDays = "0",
-    prevHours = "0",
-    prevMinutes = "0",
-    prevSeconds = "0";
-  let currentDays = "0",
-    currentHours = "0",
-    currentMinutes = "0",
-    currentSeconds = "0";
-  const twoWeeksAhead = addDays(new Date(), 14).getTime();
-  setInterval(() => {
-    // const distance = COUNT_DOWN_DATE - Date.now();
-    const distance = twoWeeksAhead - Date.now();
-    console.log(distance);
-    currentDays = toStringAndPad(Math.floor(distance / MILLIS_IN_DAY));
-    currentHours = toStringAndPad(
-      Math.floor((distance % MILLIS_IN_DAY) / MILLIS_IN_HOUR)
-    );
-    currentMinutes = toStringAndPad(
-      Math.floor((distance % MILLIS_IN_HOUR) / MILLIS_IN_MINUTE)
-    );
-    currentSeconds = toStringAndPad(
-      Math.floor((distance % MILLIS_IN_MINUTE) / MILLIS_IN_SECOND)
-    );
+// const INIT_HTML_ELEMENTS: CountdownElements = {
+//   [CountdownElementKey.outerContainer]: null,
+//   [CountdownElementKey.innerContainer]: null,
+//   [CountdownElementKey.topElement]: null,
+//   [CountdownElementKey.bottomElement]: null,
+//   [CountdownElementKey.backElement]: null,
+//   [CountdownElementKey.backBottomElement]: null,
+// };
 
-    if (daysEl && prevDays !== currentDays) {
-      //   daysEl.classList.add("time-unit__value--flipped");
-      daysEl.innerHTML = prevDays;
-      daysBottomEl?.setAttribute("data-value", currentDays);
-      daysBackEl?.setAttribute("data-value", currentDays);
-      daysBackBottomEl?.setAttribute("data-value", prevDays);
-      prevDays = currentDays;
-      removeClassAfterTimeout(daysEl, "time-unit__value--flipped", 500);
-    }
-    if (hoursEl && prevHours !== currentHours) {
-      //   hoursEl.classList.add("time-unit__value--flipped");
+const INIT_UNIT_HTML_ELEMENTS: CountdownUnitElements = {
+  [TimeUnitKey.days]: getCountdownElements(TimeUnitKey.days),
+  [TimeUnitKey.hours]: getCountdownElements(TimeUnitKey.hours),
+  [TimeUnitKey.minutes]: getCountdownElements(TimeUnitKey.minutes),
+  [TimeUnitKey.seconds]: getCountdownElements(TimeUnitKey.seconds),
+} as CountdownUnitElements;
 
-      hoursEl.innerHTML = prevHours;
-      hoursBottomEl?.setAttribute("data-value", currentHours);
-      hoursBackEl?.setAttribute("data-value", currentHours);
-      hoursBackBottomEl?.setAttribute("data-value", prevHours);
-      prevHours = currentHours;
-      removeClassAfterTimeout(hoursEl, "time-unit__value--flipped", 500);
-    }
-    if (minutesEl && prevMinutes !== currentMinutes) {
-      //   minutesEl.classList.add("time-unit__value--flipped");
-      minutesEl.setAttribute("data-value", currentMinutes);
-      minutesEl.innerHTML = currentMinutes;
-      prevMinutes = currentMinutes;
-      removeClassAfterTimeout(minutesEl, "time-unit__value--flipped", 500);
-    }
-    if (secondsEl && prevSeconds !== currentSeconds) {
-      //   secondsEl.classList.add("time-unit__value--flipped");
-      secondsEl.innerHTML = prevSeconds;
-      secondsBackEl?.setAttribute("data-value", currentSeconds);
-      secondsBottomEl?.setAttribute("data-value", currentSeconds);
-      secondsBackBottomEl?.setAttribute("data-value", prevSeconds);
+const countdownTimer = new CountdownTimer(
+  TWO_WEEKS_AHEAD,
+  Date.now(),
+  INIT_UNIT_HTML_ELEMENTS,
+  INIT_UNIT_DELTA_VALUES
+);
+updateTimer();
 
-      prevSeconds = currentSeconds;
-      removeClassAfterTimeout(secondsEl, "time-unit__value--flipped", 500);
-    }
-  }, COUNT_DOWN_INTERVAL);
-};
+// function updateTimer(requestTimestamp?: number): void {
+function updateTimer(): void {
+  console.count("updateTimer");
+  // const timeCalled = requestTimestamp || Date.now();
+  // const timeInterval = requestAnimationFrame(updateTimer);
 
-const toStringAndPad = (num: number) => {
-  return num.toString().padStart(2, "0");
-};
+  // const timeInterval = requestAnimationFrame(updateTimer);
+  // const distance = getUpdatedDistance(countdownTimer.$targetDate);
+  // if (distance < 0) {
+  //   cancelAnimationFrame(timeInterval);
+  //   return;
+  // }
 
-const removeClassAfterTimeout = (
-  _el: HTMLElement,
-  _className: string,
-  _timeout: number
-) => {
-  //   setTimeout(() => {
-  //     el.classList.remove(className);
-  //   }, timeout);
-};
-
-runCountdownTimer();
+  setTimeout(() => countdownTimer.update(), 500);
+  // countdownTimer.update();
+}
