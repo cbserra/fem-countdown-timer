59e9daa1fd7f92e7289c4aa7b4cf4d651aa165e2
Chris Serra <789723+cbserra@users.noreply.github.com>
2022-08-02 23:04:34 -0700

    feat: animated wip #2

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,39 @@
 import path from "path";
+import { resolve } from 'path'
 import { defineConfig } from "vite";
 import packageJson from "./package.json";
 
-const getPackageName = () => {
-  return packageJson.name;
-};
+// const getPackageName = () => {
+//   return packageJson.name;
+// };
 
-const getPackageNameCamelCase = () => {
-  try {
-    return getPackageName().replace(/-./g, (char) => char[1].toUpperCase());
-  } catch (err) {
-    throw new Error("Name property in package.json is missing.");
-  }
-};
+// const getPackageNameCamelCase = () => {
+//   try {
+//     return getPackageName().replace(/-./g, (char) => char[1].toUpperCase());
+//   } catch (err) {
+//     throw new Error("Name property in package.json is missing.");
+//   }
+// };
 
-const fileName = {
-  es: `${getPackageName()}.mjs`,
-  cjs: `${getPackageName()}.cjs`,
-  iife: `${getPackageName()}.iife.js`,
-};
+// const fileName = {
+//   es: `${getPackageName()}.mjs`,
+//   cjs: `${getPackageName()}.cjs`,
+//   iife: `${getPackageName()}.iife.js`,
+// };
 
 module.exports = defineConfig({
   base: "./",
   build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: getPackageNameCamelCase(),
-      formats: ["es", "cjs", "iife"],
-      fileName: (format) => fileName[format],
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+    // lib: {
+    //   entry: path.resolve(__dirname, "src/index.ts"),
+    //   name: getPackageNameCamelCase(),
+    //   formats: ["es", "cjs", "iife"],
+    //   fileName: (format) => fileName[format],
+    // },
+      },
     },
-  },
+  }
 });
